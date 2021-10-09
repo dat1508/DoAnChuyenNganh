b6b3b4d89e8b45babfa1d3e8c7339191c213ab73
truong <quangtruong.tdr012@gmail.com>
2021-10-09 23:04:33 +0700

    Finished login page

--- WebDauGia/DAO/UserDAO.cs
+++ WebDauGia/DAO/UserDAO.cs
@@ -8,5 +8,15 @@ namespace WebDauGia.DAO
     public class UserDAO
     {
         DBContext db = new DBContext();
+
+        public Boolean isUserAlreadyExisted(String userName, String password)
+        {
+            var user = db.USER.FirstOrDefault(u => u.Username == userName && u.Password == password && u.Admin == false);
+            if (user != null)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
