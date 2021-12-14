b80f4ff7de0bc6073fbfbd0f6ee8c90cce4e0a18
truong <quangtruong.tdr012@gmail.com>
2021-12-14 20:02:11 +0700

    update

--- WebDauGia/Models/BLOG.cs
+++ WebDauGia/Models/BLOG.cs
@@ -11,7 +11,8 @@ namespace WebDauGia.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class BLOG
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
